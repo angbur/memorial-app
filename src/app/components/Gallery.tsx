3a70deb59f6aa5b225dc650c91b78e85c299f178
Agnieszka Bury <agnieszka.bury.ext@unit4.com>
2025-03-30 11:52:32 +0200

    update

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -69,7 +69,7 @@ export default function Gallery({
         <p className="text-gray-500">Brak zdjęć w galerii.</p>
       )}
       {carouselIndex !== null && (
-        <div className="fixed inset-0 bg-black bg-opacity-90 flex flex-col items-center justify-center z-50">
+        <div className="fixed z-100 inset-0 bg-black bg-opacity-90 flex flex-col items-center justify-center z-50">
           <div className="relative w-full h-full flex items-center justify-center">
             <button
               className="absolute top-4 right-4 text-white text-3xl"
